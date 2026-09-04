eb2eb9a386527de581a8251fb188498109fa64e9
volostnov <nkt1c@list.ru>
2026-09-04 17:14:04 +0500

    #9 "Темы,Настройки,Поиск" /2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -10,10 +10,7 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
-    companion object {
-        const val ET_ID = "ET_ID"
-        const val ET_DEF = ""
-    }
+    lateinit var et:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,24 +21,23 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val et = findViewById<EditText>(R.id.lsr_edittext)
+        et = findViewById<EditText>(R.id.lsr_edittext)
 
         val tw = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {twOnTextChanged(et,s)}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {twOnTextChanged(s)}
             override fun afterTextChanged(s: Editable?) {}
         }
 
-        et.setOnTouchListener { v, event -> etOnTouch(v as EditText, event) }
+        et.setOnTouchListener { v, event -> etOnTouch(event) }
         et.addTextChangedListener(tw)
 
-        twOnTextChanged(et,et.text)
+        twOnTextChanged(et.text)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        val et = findViewById<EditText>(R.id.lsr_edittext)
         outState.putString(ET_ID,et.text.toString())
     }
 
@@ -49,27 +45,26 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
 
         val etText = savedInstanceState.getString(ET_ID,ET_DEF)
-        val et = findViewById<EditText>(R.id.lsr_edittext)
         et.setText(etText)
-        twOnTextChanged(et,et.text)
+        twOnTextChanged(et.text)
     }
 
-    private fun twOnTextChanged(et: EditText, s: CharSequence?) {
+    private fun twOnTextChanged(s: CharSequence?) {
         val startDrawable = getDrawable(R.drawable.ic_search14)
         val endDrawable = if (s.isNullOrEmpty()) null else getDrawable(R.drawable.ic_clear)
         et.setCompoundDrawablesWithIntrinsicBounds(startDrawable,null,endDrawable,null)
     }
 
-    private fun isTouchInDrawableEnd(et: EditText, event: MotionEvent): Boolean {
+    private fun isTouchInDrawableEnd(event: MotionEvent): Boolean {
         val drawableEnd = et.compoundDrawables[2] ?: return false
         val iconX = et.right - et.compoundPaddingEnd - drawableEnd.intrinsicWidth
         return event.x >= iconX
     }
 
-    private fun etOnTouch(et: EditText, event: MotionEvent): Boolean {
+    private fun etOnTouch(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_UP -> {
-                if (isTouchInDrawableEnd(et, event)) {
+                if (isTouchInDrawableEnd(event)) {
                     et.setText("")
                     val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(et.windowToken,0)
@@ -80,4 +75,8 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
+        const val ET_ID = "ET_ID"
+        const val ET_DEF = ""
+    }
 }
